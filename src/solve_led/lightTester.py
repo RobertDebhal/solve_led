027ae7accda2081ed365c840598b4b110f4baa21
Robert De Bhal <robert.de-bhal@ucdconnect.ie>
2018-03-01 21:12:26 +0000

    lightTester.py updated. turn_on, turn_off and switch functions added.

--- src/solve_led/lightTester.py
+++ src/solve_led/lightTester.py
@@ -4,14 +4,37 @@ Created on 1 Mar 2018
 @author: robbie
 skeleton code from Assignment3: hints
 '''
-
-class lightsTester:
+class lightTester:
     lights = None
      
     def __init__(self,N):
          
         self.lights = [[False]*N for i in range(N)]
-         
+        self.count=0
+    
+    def turn_on(self,tuple):
+        for i in range (int(tuple[0]),int(tuple[2])+1):
+            for j in range(int(tuple[1]),int(tuple[3])+1):
+                if not self.lights[i][j]:
+                    self.lights[i][j]=True
+                    self.count+=1
+                    
+    def turn_off(self,tuple):
+        for i in range (int(tuple[0]),int(tuple[2])+1):
+            for j in range(int(tuple[1]),int(tuple[3])+1):
+                if self.lights[i][j]:
+                    self.lights[i][j]=True
+                    self.count-=1
+                    
+    def switch(self,tuple):
+        for i in range (int(tuple[0]),int(tuple[2])+1):
+            for j in range(int(tuple[1]),int(tuple[3])+1):
+                if lights[i][j]:
+                    lights[i][j] = False
+                    self.count-=1
+                else:
+                    lights[i][j] = True
+                    self.count+=1
          
     def apply(self,cmd):
         if cmd == 'turn on':
@@ -23,15 +46,3 @@ class lightsTester:
         elif cmd=='switch':
             #switch
             pass
-        
-    def count(self):
-        count=0
-        for i in self.lights:
-            for j in i:
-                if j:
-                    count+=1  
-        return count
-        
-    
-         
-         
