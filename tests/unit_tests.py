521a6f4e145afb09e1bee2ad5a97bb041d92d2c5
Robert De Bhal <robert.de-bhal@ucdconnect.ie>
2018-03-08 18:38:07 +0000

    Adjusting to account for count function.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -19,7 +19,7 @@ class TestLightTesterClass(unittest.TestCase):
         lights.turn_on((600,0,600,999)) #turn on row 600
         lights.turn_on((800,0,800,999)) #turn on row 800
         lights.turn_on((800,0,800,999)) #turn on row 800 again. should do nothing.
-        self.assertTrue(lights.count==5000) #check count. Should be 5000 
+        self.assertTrue(lights.count()==5000) #check count. Should be 5000 
     
     def test_turn_off(self):
         lights=lightTester(1000) #create lights object 1000*1000
@@ -31,7 +31,7 @@ class TestLightTesterClass(unittest.TestCase):
         lights.turn_off((400,0,400,999)) #turn off row 400
         lights.turn_off((600,0,600,999)) #turn off row 600
         lights.turn_off((600,0,600,999)) #turn off row 600 again. Should do nothing.
-        self.assertTrue(lights.count==3000) #check count. Should be 3000 
+        self.assertTrue(lights.count()==3000) #check count. Should be 3000 
     
     def test_switch(self):
         lights=lightTester(1000) #create lights object 1000*1000
@@ -44,7 +44,7 @@ class TestLightTesterClass(unittest.TestCase):
         lights.turn_off((600,0,600,999)) #turn off row 600
         lights.turn_off((600,0,600,999)) #turn off row 600 again. Should do nothing.
         lights.switch((0,0,999,999)) #switch all lights. count should be 997,000
-        self.assertTrue(lights.count==997000) #check count. Should be 997,000 
+        self.assertTrue(lights.count()==997000) #check count. Should be 997,000 
         
         
     def test_apply(self):
@@ -56,7 +56,7 @@ class TestLightTesterClass(unittest.TestCase):
                                                    #count should now be 997,500
         lights.apply(('turn off', '900', '0', '1000', '1000')) #should correct for light out of range.
                                                    #count should now be 897,500
-        self.assertTrue(lights.count==897500)
+        self.assertTrue(lights.count()==897500)
         
 class ConsoleScriptTest:
     def check_file(self):
