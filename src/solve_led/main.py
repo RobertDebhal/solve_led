ab3cf0c37726b59dfbd00066d8c73f9189e455f2
Robert De Bhal <robert.de-bhal@ucdconnect.ie>
2018-03-08 21:18:45 +0000

    Updating command line option: --input/-I

--- src/solve_led/main.py
+++ src/solve_led/main.py
@@ -11,16 +11,17 @@ import solve_led.parse_file as parse_file, sys
 def main():
     #source: https://stackoverflow.com/questions/40753946/python-setup-tools-console-scripts-with-arguments 
     arg1, arg2 = sys.argv[1], sys.argv[2]
-    print(arg1, arg2)
     
-    instructions = parse_file.ParseFile(arg2)
+    if arg1=='--input'or arg1=='-I':
+        instructions = parse_file.ParseFile(arg2)
     
-    lights = lightTester(int(instructions.lines[0]))
+        lights = lightTester(int(instructions.lines[0]))
     
-    for i in range(1,len(instructions.lines)):
-        lights.apply(instructions.lines[i])
+        for i in range(1,len(instructions.lines)):
+            lights.apply(instructions.lines[i])
     
-    print("#occupied:",lights.count()) 
-    
+        print("#occupied:",lights.count()) 
+    else:
+        print(arg2+" is not recognised as a valid option")
 if __name__ == '__main__':
     main()
