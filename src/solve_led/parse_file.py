329db0cd3f6af7dd94460c3b431a6f2f9573160d
Robert De Bhal <robert.de-bhal@ucdconnect.ie>
2018-03-09 15:28:04 +0000

    updating parse_file.py to satisfy unit tests

--- src/solve_led/parse_file.py
+++ src/solve_led/parse_file.py
@@ -29,22 +29,21 @@ class ParseFile:
         else:
             try:
                 self.file = url.urlopen(filename)
+                self.lines = self.file.readlines()
+                for i in range(len(self.lines)):
+                    self.lines[i] = self.lines[i].decode('utf-8')
+                    if valid.match(self.lines[i]):
+                        self.lines[i]=(valid.match(self.lines[i]).group(1),
+                                       valid.match(self.lines[i]).group(2),
+                                       valid.match(self.lines[i]).group(3),
+                                       valid.match(self.lines[i]).group(4),
+                                       valid.match(self.lines[i]).group(5))
             except ValueError:
                 print("Please type a valid URL or filename")
-                return None
-            except HTTPError:
+            except url.HTTPError:
                 print("Please type a valid URL or filename")
-                return None
                 
-            self.lines = self.file.readlines()
-            for i in range(len(self.lines)):
-                self.lines[i] = self.lines[i].decode('utf-8')
-                if valid.match(self.lines[i]):
-                    self.lines[i]=(valid.match(self.lines[i]).group(1),
-                                   valid.match(self.lines[i]).group(2),
-                                   valid.match(self.lines[i]).group(3),
-                                   valid.match(self.lines[i]).group(4),
-                                   valid.match(self.lines[i]).group(5))
+            
             
             
     def iterate(self):
